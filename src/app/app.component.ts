75d4447108d145b81bba43f1fa1b1a83c43c4bc1
Tom Vrijmoet <tfjvrijmoet@hotmail.com>
2023-11-21 14:18:17 +0100

    Added "suggest username" functionality

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -14,6 +14,25 @@ export class AppComponent {
 
   suggestUserName() {
     const suggestedName = 'Superuser';
+
+    // This is a possible way to set the value of a form using typeScript
+    // When using a form.setValue, you MUST pass a value for every form field
+    // this.signupForm.setValue({
+    //   userData: {
+    //     username: suggestedName,
+    //     email: ''
+    //   },
+    //   secret: 'pet',
+    //   questionAnswer: '',
+    //   gender: 'male'
+    // })
+
+    // This can be used to update 1 value in the form
+    // It uses the form.form.patchValue, with which you can update the value of a specific field
+    this.signupForm.form.patchValue({
+      userData: {
+        username: suggestedName
+      }})
   }
 
   // onSubmit(form: NgForm){
