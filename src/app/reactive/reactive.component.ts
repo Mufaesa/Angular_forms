13eee8f7a329af375a58ffa695cca14aa1714fd0
Tom Vrijmoet <tfjvrijmoet@hotmail.com>
2023-11-27 11:33:08 +0100

    Added reactive Form

--- src/app/reactive/reactive.component.ts
+++ src/app/reactive/reactive.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { FormArray, FormControl, FormGroup, Validators } from '@angular/forms';
+import { Observable } from 'rxjs';
 
 @Component({
   selector: 'app-reactive',
@@ -6,10 +8,79 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./reactive.component.css']
 })
 export class ReactiveComponent implements OnInit {
+  genders = ['male', 'female'];
+  myForm: FormGroup
+  forbiddenUserNames = ['Chris', 'Anna'];
 
-  constructor() { }
+  // The form should be created before actually displaying the component. 
+  // That's why we're creating it in the ngOnInit
+  ngOnInit() {
+    this.myForm = new FormGroup({
+      userData: new FormGroup({
+        username: new FormControl(null, [
+          Validators.required,
+          this.forbiddenNames.bind(this),
+        ]),
+        email: new FormControl(
+          null,
+          [Validators.required, Validators.email],
+          this.forbiddenEmails
+        ),
+      }),
+      gender: new FormControl('male'),
+      hobbies: new FormArray([]),
+    });
+    // this.signupForm.valueChanges.subscribe(
+    //   (value) => console.log(value)
+    // );
+    this.myForm.statusChanges.subscribe((status) => console.log(status));
+    this.myForm.setValue({
+      userData: {
+        username: 'Max',
+        email: 'max@test.com',
+      },
+      gender: 'male',
+      hobbies: [],
+    });
+    this.myForm.patchValue({
+      userData: {
+        username: 'Anna',
+      },
+    });
+  }
 
-  ngOnInit(): void {
+  getControls() {
+    return (<FormArray>this.myForm.get('hobbies')).controls;
+  }
+
+  onAddHobby(){
+    const control = new FormControl(null, Validators.required);
+    (<FormArray>this.myForm.get('hobbies')).push(control);
+  }
+
+  onSubmit() {
+    console.log(this.myForm);
+    this.myForm.reset();
+  }
+
+  forbiddenNames(control: FormControl): { [s: string]: boolean } {
+    if (this.forbiddenUserNames.indexOf(control.value) !== -1) {
+      return { nameIsForbidden: true };
+    }
+    return null;
+  }
+
+  forbiddenEmails(control: FormControl): Promise<any> | Observable<any> {
+    const promise = new Promise<any>((resolve, reject) => {
+      setTimeout(() => {
+        if (control.value === 'test@test.com') {
+          resolve({ emailIsForbidden: true });
+        } else {
+          resolve(null);
+        }
+      }, 1500);
+    });
+    return promise;
   }
 
 }
