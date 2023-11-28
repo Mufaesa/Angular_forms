1cb7c6ea7f0624b1a8f3f70713fa549bdd8a1584
Tom Vrijmoet <tfjvrijmoet@hotmail.com>
2023-11-28 15:10:56 +0100

    Added custom syncronous and async validators

--- src/app/reactive-assignment/reactive-assignment.component.ts
+++ src/app/reactive-assignment/reactive-assignment.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
-import { FormControl, FormGroup } from '@angular/forms';
+import { AbstractControl, FormControl, FormGroup, ValidationErrors, Validators } from '@angular/forms';
+import { Observable } from 'rxjs';
 
 @Component({
   selector: 'app-reactive-assignment',
@@ -8,14 +9,15 @@ import { FormControl, FormGroup } from '@angular/forms';
 })
 export class ReactiveAssignmentComponent implements OnInit {
   statusOptions = ['Stable', 'Critical', 'Finished'];
-  ReactiveForm: FormGroup;  
+  ReactiveForm: FormGroup;
+  forbiddenName: string = "Test";
 
 
   constructor() { }
 
   ngOnInit(): void {
     this.ReactiveForm = new FormGroup({
-      projectName: new FormControl(null),
+      projectName: new FormControl(null, [Validators.required], this.forbiddenNameValidatorAsync),
       email: new FormControl(null),
       status: new FormControl(null)
     });
@@ -23,6 +25,26 @@ export class ReactiveAssignmentComponent implements OnInit {
 
   onSubmit(){
     console.log(this.ReactiveForm);
+  }
+
+  forbiddenNameValidator = (control: AbstractControl): ValidationErrors | null => {
+    if (control.value && control.value.toLowerCase() === this.forbiddenName.toLowerCase()) {
+      return { forbiddenProjectName: { value: control.value } };
+    }
+    return null;
+  }
+
+  forbiddenNameValidatorAsync(control: FormControl): Promise<any> | Observable<any> {
+    const promise = new Promise<any>((resolve, reject) => {
+      setTimeout(() => {
+        if (control.value === 'Test') {
+          resolve({ forbiddenProjectName: true });
+        } else {
+          resolve(null);
+        }
+      }, 1500);
+    });
+    return promise;
   }
 
 }
